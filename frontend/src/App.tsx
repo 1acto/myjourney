8ebab346882b1a06e944eb1315ce785b3f80c019
Namtan26 <164167951+Namtan26@users.noreply.github.com>
2025-10-05 22:19:08 +0700

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,6 +12,7 @@ import BranchEditPage from "@/pages/branch/edit/BranchesEditPage";
 import BranchInfo from "@/pages/branch/info/BranchInfo";
 import TagsPage from "@/pages/setting/TagsPage";
 
+
 axios.defaults.withCredentials = true;
 axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 
